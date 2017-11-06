45e2c92c625a24c30fc7acfc436315ce05b5f898
dmayes403 <davidrmayes@gmail.com>
2017-11-05 21:52:05 -0700

    working with axios all

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import _ from 'lodash';
 import { SEARCH_MOVIES, MOVIE_DETAILS } from './types';
 
 const movieDBKEY = process.env.REACT_APP_MOVIE_DB_KEY;
@@ -6,8 +7,15 @@ const movieDBKEY = process.env.REACT_APP_MOVIE_DB_KEY;
 export const searchMovies = (movieTitle) => async dispatch => {
     const movieBaseUrl = `https://api.themoviedb.org/3/search/movie?api_key=${movieDBKEY}&language=en-US&query=${movieTitle}`;
     const res = await axios.get(movieBaseUrl);
-    console.log(res.data.results)
-    dispatch({ type: SEARCH_MOVIES, payload: res.data.results });
+    // console.log(res.data.results)
+    const movies = res.data.results.filter(movie => {
+        if (movie.poster_path) {
+            return movie;
+        } else {
+            return false;
+        }
+    })
+    dispatch({ type: SEARCH_MOVIES, payload: movies });
 };
 
 export const getMovieDetails = (movieId) => async dispatch => {
@@ -15,12 +23,23 @@ export const getMovieDetails = (movieId) => async dispatch => {
     const getVideosURL = `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${movieDBKEY}&language=en-US`
     const getRatingsURL = `https://api.themoviedb.org/3/movie/${movieId}/release_dates?api_key=${movieDBKEY}&language=en-US`
 
-    const details = await axios.get(getDetailsURL);
-    const videos = await axios.get(getVideosURL);
-    const ratings = await axios.get(getRatingsURL);
-    console.log(details);
-    console.log(videos);
-    console.log(ratings);
+    // const details = await axios.get(getDetailsURL);
+    // const videos = await axios.get(getVideosURL);
+    // const ratings = await axios.get(getRatingsURL);
+    // console.log(details);
+    // console.log(videos);
+    // console.log(ratings);
     // dispatch({ type: MOVIE_DETAILS, payload: res.data.results });
+
+    axios.all([
+        axios.get(getVideosURL),
+        axios.get(getRatingsURL),
+        axios.get(getDetailsURL)
+    ]).then(axios.spread((videos, ratings, details) => {
+        const rating = _.find(ratings.data.results, {iso_3166_1: "US"})
+        const data = [videos.data.results, rating.release_dates[rating.release_dates.length-1].certification, details.data];
+        console.log(data);
+        dispatch({ type: MOVIE_DETAILS, payload: data });
+    }))
 
 }
--- client/src/components/NewShowingCreate.js
+++ client/src/components/NewShowingCreate.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import { reduxForm, Field } from 'redux-form';
 import { connect } from 'react-redux';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+// import { BrowserRouter as Router, Route } from 'react-router-dom'
 import * as actions from '../actions';
 import '../styles/newShowingCreate.css';
 
@@ -14,27 +14,49 @@ class NewShowingCreate extends Component {
     }
 
     render() {
-        return (
-            <div className="create-showing-container">
-                <div>
+        const { movieDetails } = this.props;
+        var detailContainerStyles = {
+            display: 'flex',
+            flexDirection: 'row'
+        }
+
+        if (this.props.movieDetails.length > 0) {
+            return (
+                <div className="create-showing-container">
+                    <div style={detailContainerStyles}>
+                        <img src={ `http://image.tmdb.org/t/p/w342//${movieDetails[2].poster_path}` }
+                            style={{width: '35%'}}/>
+                        <div className="description-container">
+                            <h2 style={{margin: 'auto', textAlign: 'center'}}>{movieDetails[2].title}</h2>
+                            <p>Rating: {movieDetails[1]}</p>
+                        </div>
+                    </div>
+                    <div className="videos-container">
+                        {console.log(this.props)}
+                        {this.renderVideos()}
+                    </div>
                 </div>
-                <div className="videos-container">
-                    {this.renderVideos()}
-                </div>
-            </div>
-        )
+            )
+        } else {
+            return (
+                <div>Loading... </div>
+            );
+        }
     }
 
     renderVideos() {
-        return this.props.movieDetails.map(detail => {
-            return (
-                <div key={detail.key} className="single-video-container">
-                    <iframe width="250"
-                        src={ `https://www.youtube.com/embed/${detail.key}` } style={{border: 'none'}}>
-                    </iframe>
-                </div>
-            );
-        });
+        if (this.props.movieDetails[0]) {
+            return this.props.movieDetails[0].map(detail => {
+                return (
+                    <div key={detail.key} className="single-video-container">
+                        <iframe width="250"
+                            src={ `https://www.youtube.com/embed/${detail.key}` } style={{border: 'none'}}
+                            allowFullScreen="allowfullscreen">
+                        </iframe>
+                    </div>
+                );
+            });
+        }
     }
 }
 
