6da6a544f1d644c5a86b01a592ca35f8bd246059
dmayes403 <davidrmayes@gmail.com>
2017-12-05 23:42:02 -0700

    updating theater list

--- client/src/components/admin/Admin.js
+++ client/src/components/admin/Admin.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import './admin.css';
 
 import UserPrivileges from '../user-privileges/userPrivileges';
+import TheaterListUpdate from '../theater-list-update/theaterListUpdate';
 
 class Admin extends Component {
     render() {
         return (
             <div className="main-container">
                 <div className="inner-main-container" style={{paddingTop: '50px'}}>
-                    <div>
+                    <div className="flex-row">
                         <UserPrivileges />
+                        <TheaterListUpdate />
                     </div>
                 </div>
             </div>
--- client/src/components/theater-list-update/theaterListUpdate.js
+++ client/src/components/theater-list-update/theaterListUpdate.js
@@ -1,12 +1,53 @@
 import React, { Component } from 'react';
 import { reduxForm, Field, reset, change } from 'redux-form';
 import { connect } from 'react-redux';
+import { Card } from 'material-ui/Card';
 import * as actions from '../../actions';
 
 import './theaterListUpdate.css';
 
 class TheaterListUpdate extends Component {
+    state = {
+        theaterList: []
+    }
+    componentDidMount() {
+        this.props.getTheaterList().then(() => {
+            this.setState({ theaterList: this.props.theaterList });
+        });
+    }
 
+    render() {
+        return (
+            <Card className="theater-list-container">
+                <form onSubmit={this.props.handleSubmit((values) => console.log(values))}>
+                    <Field
+                        type="text"
+                        name="theaterName"
+                        component="input"
+                        placeholder="Add Theater"
+                    />
+                </form>
+                {console.log(this.props.theaterList)}
+                {this.props.theaterList.map(theater => {
+                    return (
+                        <div key={theater._id}>{theater.room}</div>
+                    )
+                })}
+            </Card>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    // console.log(state.form.theaterListUpdate);
+    return { 
+        theaterList: state.theaterList,
+        formValues: state.form.theaterListUpdate
+    };
+}
+
+const afterSubmit = (result, dispatch) => {
+    dispatch(reset('theaterListUpdate'));
 }
 
 TheaterListUpdate = connect(mapStateToProps, actions)(TheaterListUpdate);
