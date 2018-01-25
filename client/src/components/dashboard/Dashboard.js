64b59d7949811c0474d77ad9343d1e614eb80c3e
dmayes403 <davidrmayes@gmail.com>
2018-01-24 19:36:28 -0700

    starting movie showings media queries

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -7,7 +7,6 @@ import { Link } from 'react-router-dom';
 
 import './dashboard.css';
 
-
 class Dashboard extends Component {
     state = { 
         focusedDateIndex: 0,
--- client/src/components/movie-details/MovieDetails.js
+++ client/src/components/movie-details/MovieDetails.js
@@ -5,7 +5,6 @@ import { Card } from 'material-ui/Card';
 import * as actions from '../../actions';
 import './movieDetails.css';
 
-// adding media queries
 class MovieDetails extends Component {
     componentDidMount() {
         this.props.getMovieDetails(this.props.match.params.id);
